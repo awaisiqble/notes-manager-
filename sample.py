e5272fa978e587930c020a3df776a9e6342d03f2
awaisiqbl <boocsfreda@gmail.com>
2025-03-01 23:59:54 +0500

    this is for practice

---
+++ sample.py
@@ -0,0 +1,50 @@
+a=1+2
+a+=5
+print(a)
+def addd1(x,y) :
+    print(x +y)
+    x = 6
+y = 9
+addd1(4,7)
+
+def awais(f,g) :
+    print(f-g)
+    f = 1
+    g= 7
+    awais(f,g) 
+    awais(5,3)
+print("this is double back slash \\\\")
+print("these are mountains /\\/\\/\\/\\/\\")
+print("he is\t awesome")
+print(r"\" \n \t")
+print('\\" \\n \\t')
+
+u1 = "a"
+u2 = "b"
+u = u1 +" " + u2
+print(u)
+df = u + str(4)
+print(df)
+name = input("tell ur name")
+print("hello" + name)
+import datetime
+print(datetime.datetime.now())
+print(datetime.datetime.now().date())
+print(datetime.datetime.now().time())
+
+
+
+
+
+u1 = "a"
+u2 = "b"
+u = u1 +" " + u2
+print(u)
+df = u + str(4)
+print(df)
+name = input("tell ur name")
+print("hello" + name)
+import datetime
+print(datetime.datetime.now())
+print(datetime.datetime.now().date())
+print(datetime.datetime.now().time())
